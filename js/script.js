3c69e9d7783d4c4a1229ddccc39a1b96d9369049
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-03-14 11:39:54 +0100

    while funzionante

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let array= [ ];
+let sumNum= 0
+while (sumNum < 50){
+    let userNum= parseInt(prompt("inserisci un numero"))
+    //while(isNaN(userNum)){
+        //alert("il numero inserito non è valido")
+    //}
+    sumNum += userNum
+}
