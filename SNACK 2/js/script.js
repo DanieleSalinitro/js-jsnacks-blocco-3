6c918b21d2a03408f9cc8ab34a400d95c1e05a39
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-03-14 12:21:06 +0100

    svolgimento js

--- SNACK 2/js/script.js
+++ SNACK 2/js/script.js
@@ -1,1 +1,19 @@
 //SNACK 2
+
+//funzione che genera numeri random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let n = parseInt(prompt("Inserisci un numero"));
+
+function genArray (){
+    let array=[];
+    for (i=0; i<10; i++){
+        array.push(getRndInteger(1,100))
+    }
+    return array;
+}
+for (i = 0; i<n; i++){
+
+}
